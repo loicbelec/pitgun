82cdc3d9e19db4550016ea305d91a5d433848418
Loïc Bélec <github@loicbelec.com>
2026-07-22 21:39:15 +0200

    fix(racing): preserve browser catalog exports

--- crates/pitgun-solver/src/lib.rs
+++ crates/pitgun-solver/src/lib.rs
@@ -7,15 +7,17 @@ pub mod rng;
 
 pub use pitgun_racing_simulator::evidence;
 pub use pitgun_racing_simulator::{
-    AeroParams, CatalogSnapshot, ChassisParams, CircuitDetail, Driver, DriverEffects, EngineDetail,
-    EngineParams, PitPlan, PitStop, PitStrategyConfig, RaceOutput, RacingWorkload,
-    RacingWorkloadError, ResampledTelemetry, RunRaceInput, RunRaceRequest, RunSimulationRequest,
-    SessionConfig, SessionRunOutput, SessionRunRequest, SessionRunResult, SimConfig,
-    SimulationRequest, SimulationResult, SimulationSolution, SolverTrackProfile, StandingEntry,
-    StandingStatus, TelemetryEnvelope, TireParams, Track, Tuning, VehicleParams, VehicleState,
+    AeroParams, BrowserCircuitCatalogEntry, CatalogSnapshot, ChassisParams, CircuitDetail, Driver,
+    DriverCatalogEntry, DriverEffects, EngineDetail, EngineParams, PitPlan, PitStop,
+    PitStrategyConfig, RaceOutput, RacingWorkload, RacingWorkloadError, ResampledTelemetry,
+    RunRaceInput, RunRaceRequest, RunSimulationRequest, SessionConfig, SessionRunOutput,
+    SessionRunRequest, SessionRunResult, SimConfig, SimulationRequest, SimulationResult,
+    SimulationSolution, SolverTrackProfile, StandingEntry, StandingStatus, TelemetryEnvelope,
+    TireCatalogEntry, TireParams, Track, Tuning, VehicleCatalogEntry, VehicleParams, VehicleState,
     apply_driver_to_tire, apply_tuning, best_power_at_speed, catalog_snapshot, derating_factor,
-    driver_effects, effective_mu, get_circuit, get_engine, list_circuits, list_engines,
-    power_kw_from_rpm, resample_solution, rpm_from_speed_gear, run_race, run_sessions, solve,
+    driver_effects, effective_mu, get_circuit, get_engine, list_browser_circuits, list_circuits,
+    list_drivers, list_engines, list_tires, list_vehicles, power_kw_from_rpm, resample_solution,
+    rpm_from_speed_gear, run_race, run_sessions, solve,
 };
 use wasm_bindgen::prelude::*;
 
@@ -62,4 +64,19 @@ pub fn list_engines_json() -> String {
 #[wasm_bindgen]
 pub fn get_engine_json(engine_id: String) -> String {
     pitgun_racing_simulator::get_engine_json(engine_id)
+}
+
+#[wasm_bindgen]
+pub fn list_drivers_json() -> String {
+    pitgun_racing_simulator::list_drivers_json()
+}
+
+#[wasm_bindgen]
+pub fn list_vehicles_json() -> String {
+    pitgun_racing_simulator::list_vehicles_json()
+}
+
+#[wasm_bindgen]
+pub fn list_tires_json() -> String {
+    pitgun_racing_simulator::list_tires_json()
 }
